633816f0e9cbf10c4642084a9026e1bbe8fe9930
Metz I <128658219+metz-i@users.noreply.github.com>
2024-02-24 21:48:51 -0500

    added components which display images that respond to user clicks

--- src/App.js
+++ src/App.js
@@ -1,18 +1,78 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import AnimalShow from './AnimalShow';
 
-
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+    return animals[Math.floor(Math.random() * animals.length)];
+}
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [animals, setAnimals] = useState([]);
 
     const handleClick = () => {
-       setCount(count + 1);
+        setAnimals([...animals, getRandomAnimal()]);
     };
 
-    return <div>
-        <button onClick={handleClick}>Add animal</button>
-        <div>Number of animals: `{count}</div>
-    </div>
+    const renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key={index} />;
+    });
+
+    return (
+        <div>
+            <button onClick={handleClick}>Add animal</button>
+            {renderedAnimals}
+        </div>
+    );
 }
 
 export default App;
+
+
+
+
+
+//import AnimalShow from './AnimalShow';
+//
+//function getRandomAnimal() {
+//    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+//    return animals[Math.floor(Math.random() * animals.length)];
+//}
+//
+//
+//
+//function App() {
+//    const handleClick = () => {
+//        setAnimals([...animals, getRandomAnimal()]);
+//     } ;
+//
+//     const renderedAnimals = animals.map((animal, index) => {
+//        return <AnimalShow type={animal} key={index} />
+//     })
+////
+//    return (<div>
+//        <button onClick={handleClick}>Add animal</button>
+//        <div>{animals}</div>
+//    </div>
+//    );
+//    };
+//
+//
+//export default App;
+
+//function App() {
+    // const [animals, setAnimals] = useState([])
+
+    // function makeArray() {
+    //     return [1, 10, 32, 40];
+    // }
+
+    // const myArray = makeArray();
+    // const firstElement = myArray[0];
+    // const secondElement = myArray[1];
+    //const[firstElement, secondElement] = makeArray()
+    //console.log(firstElement, secondElement);
+
+    // const [count, setCount] = useState([]);
+   //  const [value, setValue] = useState(0);
+
+    // console.log(useState(50));
